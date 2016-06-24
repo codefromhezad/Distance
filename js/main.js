3edc5f17c24a436783f402105a96b28c53199ba5
Pierrick Varin <pierrick.varin@glitchstudio.com>
2016-06-24 17:32:02 +0200

    Base template for fragment shader coding is ready

--- js/main.js
+++ js/main.js
@@ -5,23 +5,38 @@ $( function() {
 
 	var VIEW_ANGLE = 45,
 	    ASPECT = WIDTH / HEIGHT,
-	    NEAR = 0.1,
-	    FAR = 10000;
+	    NEAR = 1,
+	    FAR = 1000;
 
 	var $container = $('#main-screen');
 
-	var renderer = new THREE.WebGLRenderer();
-	var camera = new THREE.PerspectiveCamera(
-	                   VIEW_ANGLE,
-	                   ASPECT,
-	                   NEAR,
-	                   FAR );
+	var renderer = new THREE.WebGLRenderer({ antialias: true });
+	var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 
 	var scene = new THREE.Scene();
 
 	camera.position.z = 300;
 
 	renderer.setSize(WIDTH, HEIGHT);
-	
+
 	$container.append(renderer.domElement);
+
+	/* LOADING SHADERS */
+	var shaderMaterial = new THREE.ShaderMaterial({
+	    vertexShader:   $('#vertexshader').text(),
+	    fragmentShader: $('#fragmentshader').text(),
+	    depthWrite: false,
+		depthTest: false
+	});
+
+	/* FULLSCREEN QUAD */
+
+	var quad = new THREE.Mesh(
+		new THREE.PlaneGeometry(2, 2),
+		shaderMaterial
+	);
+
+	scene.add(quad);
+
+	renderer.render(scene, camera);
 });
