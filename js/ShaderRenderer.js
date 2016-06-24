720b73f9427633aedacac107ffc6163fa6f41e5b
Pierrick <pierrick.varin@zagett.com>
2016-06-24 23:39:44 +0200

    Huge refactoring of the JS renderer

--- js/ShaderRenderer.js
+++ js/ShaderRenderer.js
@@ -1,60 +1,111 @@
-var ShaderRenderer = function(screen_id, width, height, vertex_file, fragment_file) {
-	this.width = width;
-	this.height= height;
-	this.screen_id = screen_id;
+var MAX_PARSING_RECURSIONS = 50;
 
-	/* INIT SCENE / RENDERER */
-	var VIEW_ANGLE = 45,
-	    ASPECT = this.width / this.height,
-	    NEAR = 1,
-	    FAR = 1000;
+var ShaderRenderer = function() {
 
-	var renderer = new THREE.WebGLRenderer({ antialias: true });
-	var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+	this.init = function(screen_id, width, height) {
 
-	var scene = new THREE.Scene();
+		/* Init renderer DOM and data */
+		this.width = width;
+		this.height= height;
+		this.canvas = document.getElementById(screen_id);
 
-	camera.position.z = 300;
+		if( this.canvas.length == 0 ) {
+			console.error("Can't find any canvas with id '"+screen_id+"'");
+			return;
+		}
 
-	renderer.setSize(this.width, this.height);
+		/* Init scene / renderer */
+		this.renderer = new THREE.WebGLRenderer({canvas: this.canvas});
+		this.renderer.setSize(this.width, this.height);
 
-	$('#' + screen_id).append(renderer.domElement);
+		this.camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 1000);
+		this.scene = new THREE.Scene();
 
+		/* Init shader constants as uniforms */
+		this.uniforms = {
+		    u_resolution: { type: 'vec2', value: {x: this.width, y: this.height} },
+		    u_fov: { type: 'f', value: 1 }
+		};
 
-	/* SHADER ATTRIBUTES */
-	this.uniforms = {
-	    u_resolution: {
-	        type: 'vec2',
-	        value: {x: this.width, y: this.height}
-	    },
-	    u_fov: {
-	    	type: 'f',
-	    	value: 1
-	    }
-	};
+		/* Init shader material */
+		this.shaderMaterial = new THREE.ShaderMaterial({
+		    vertexShader: "void main() { gl_Position = vec4(position, 1.0); }",
+		    fragmentShader: "void main() { gl_FragColor = vec4(0.0); }",
+		    uniforms: this.uniforms,
+		    depthWrite: false,
+			depthTest: false
+		});
 
-	/* SHADERS LOADING */
-	var that = this;
-	$.get(vertex_file, function(vertex_data) {
-		$.get(fragment_file, function(fragment_data) {
-			that.shaderMaterial = new THREE.ShaderMaterial({
-			    vertexShader: vertex_data,
-			    fragmentShader: fragment_data,
-			    uniforms: that.uniforms,
-			    depthWrite: false,
-				depthTest: false
+		/* Fullscreen quad used as a screen for the fragment shader */
+		this.quad = new THREE.Mesh(
+			new THREE.PlaneGeometry(2, 2),
+			this.shaderMaterial
+		);
+		this.scene.add(this.quad);
+	}
+
+	this.loadFile = function(file_path, done_callback) {
+		$.get(file_path, function(response) {
+			done_callback(response);
+		}).fail( function(response) {
+			console.error("Can't load '" + file_path + "': "+
+				"A " + response.status + " error was returned by the server");
+		});
+	}
+
+	this.parseDistanceShader = function(shader_code, done_callback, recursion_level) {
+
+		if( recursion_level === undefined ) {
+			recursion_level = 0;
+		}
+
+		if( recursion_level >= MAX_PARSING_RECURSIONS ) {
+			console.error("Too much recursion while parsing the shader code. Please check for syntax errors");
+			return;
+		}
+
+		/* Parse @import directive */
+		var import_match = shader_code.match(/\@import\s*\(\s*(.+)\s*\)/i);
+		if( import_match ) {
+			var directive_length = import_match[0].length;
+			var directive_file = import_match[1];
+			var directive_index = import_match.index;
+			
+			var that = this;
+			this.loadFile(directive_file, function(imported_data) {
+				shader_code = shader_code.substring(0, directive_index) + 
+					imported_data + 
+					shader_code.substring(directive_index + directive_length);
+
+				that.parseDistanceShader(shader_code, done_callback, recursion_level + 1);
 			});
 
-			/* FULLSCREEN QUAD */
-			var quad = new THREE.Mesh(
-				new THREE.PlaneGeometry(2, 2),
-				that.shaderMaterial
-			);
+			return;
+		}
 
-			/* Render */
-			scene.add(quad);
+		/* Remove eventual non ascii characters in the shader code */
+		shader_code = shader_code.replace(/[^\x00-\x7F]/g, "");
 
-			renderer.render(scene, camera);
+		/* Everything parsed, let's execute the done callback */
+		if( done_callback ) {
+			done_callback(shader_code);
+		}
+	}
+
+	this.loadFragmentShader = function(fragment_shader_file) {
+		var that = this;
+
+		this.loadFile(fragment_shader_file, function(fragment_data) {
+			that.parseDistanceShader(fragment_data, function(parsed_fragment) {
+				that.shaderMaterial.fragmentShader = parsed_fragment;
+				that.shaderMaterial.needsUpdate = true;
+
+				that.render();
+			});
 		});
-	});
+	}
+
+	this.render = function() {
+		this.renderer.render(this.scene, this.camera);
+	}
 }
--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -4,14 +4,19 @@ uniform float u_fov;
 const float g_rmEpsilon = 0.0001;
 const int maxSteps = 32;
 
-float spherePrimitive(vec3 p, float radius) {
-    return length(p) - radius;
+@import(shaders/lib/primitives.glsl)
+
+float distanceField(vec3 p) {
+    return max(
+        -spherePrimitive(p, 0.6),
+        boxPrimitive(p, vec3(0.5))
+    );
 }
 
 void main() {
-    vec3 eye = vec3(0, 0, -1);
-    vec3 up = vec3(0, 1, 0);
-    vec3 right = vec3(1, 0, 0);
+    vec3 eye = vec3(0.0, 0.0, -1);
+    vec3 up = vec3(0, 1.0, 0.0);
+    vec3 right = vec3(1.0, 0.0, 0.0);
 
     float u = gl_FragCoord.x * 2.0 / u_resolution.x - 1.0;
     float v = gl_FragCoord.y * 2.0 / u_resolution.y - 1.0;
@@ -22,13 +27,11 @@ void main() {
 
     float t = 0.0;
     
-    for(int i = 0; i < maxSteps; ++i)
-    {
+    for(int i = 0; i < maxSteps; ++i) {
         vec3 p = eye + ro + rd * u_fov * t;
-        float d = spherePrimitive(p, 0.5);
+        float d = distanceField(p);
 
-        if(d < g_rmEpsilon)
-        {
+        if(d < g_rmEpsilon) {
             color = vec4(1.0); // Sphere color
             break;
         }
