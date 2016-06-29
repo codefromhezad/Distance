7f256bf7a2a11fb829842fa8e392e3d71aaca3b8
Pierrick Varin <pierrick.varin@glitchstudio.com>
2016-06-29 10:53:23 +0200

    Removed all non necessary interface stuff

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -71,7 +71,27 @@ float boxPrimitive( vec3 p, vec3 b ) {
 
 /* DISTANT FIELD FUNCTION SETUP */
 
-@var(distant_field_function)
+float inf = 999999.0;
+
+float distanceField(vec3 p) {
+    float d = boxPrimitive(p,vec3(1.0));
+
+    float s = 1.0;
+    for( int m=0; m<4; m++ ) {
+        vec3 a = mod( p*s, 2.0 )-1.0;
+        s *= 3.0;
+        vec3 r = abs(1.0 - 3.0*abs(a));
+
+        float da = max(r.x,r.y);
+        float db = max(r.y,r.z);
+        float dc = max(r.z,r.x);
+        float c = (min(da,min(db,dc))-1.0)/s;
+
+        d = max(d,c);
+    }
+
+    return d;
+}
 
 
 
